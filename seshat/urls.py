197b6e778b9babef333df617a752f458f9125782
Majid Benam <benam.mjd@gmail.com>
2022-04-26 12:55:36 +0200

    changed where static is added

--- seshat/urls.py
+++ seshat/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,9 +26,8 @@ urlpatterns = [
     path('crisisdb/', include('seshat.apps.crisisdb.urls')),
 
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += [
-    # ...
     path('__debug__/', include('debug_toolbar.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
