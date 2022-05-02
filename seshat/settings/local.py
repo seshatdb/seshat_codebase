462dbe74e5e05758be07cdd88b2de4deea9144ab
Majid Benam <benam.mjd@gmail.com>
2022-05-02 16:22:43 +0200

    removed debug_toolbar from locals and added to the prod and removed from prod again

--- seshat/settings/local.py
+++ seshat/settings/local.py
@@ -4,7 +4,7 @@ from .base import *
 
 INSTALLED_APPS += ["debug_toolbar"]
 
-MIDDLEWARE.insert(0, "debug_toolbar.middleware.DebugToolbarMiddleware")
+#MIDDLEWARE.insert(0, "debug_toolbar.middleware.DebugToolbarMiddleware")
 
 
 # Databases
