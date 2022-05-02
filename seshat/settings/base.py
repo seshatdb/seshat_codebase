e67eed4c98e2e651a010f8476ad60d7af092ef34
Majid Benam <benam.mjd@gmail.com>
2022-05-02 12:43:22 +0200

    minor edits to the login page

--- seshat/settings/base.py
+++ seshat/settings/base.py
@@ -69,6 +69,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 
 ]
 
