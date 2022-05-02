07df8c7294d8dba2eb1fa49755454a9a3c23eee7
Majid Benam <benam.mjd@gmail.com>
2022-05-02 19:14:29 +0200

    added senty_sdk minus

--- seshat/settings/production.py
+++ seshat/settings/production.py
@@ -10,17 +10,17 @@ from .base import *
 # SECURITY SETTINGS
 # ==============================================================================
 
-CSRF_COOKIE_SECURE = True
-CSRF_COOKIE_HTTPONLY = True
+# CSRF_COOKIE_SECURE = True
+# CSRF_COOKIE_HTTPONLY = True
 
-SECURE_HSTS_SECONDS = 60 * 60 * 24 * 7 * 52  # one year
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-SECURE_SSL_REDIRECT = True
-SECURE_BROWSER_XSS_FILTER = True
-SECURE_CONTENT_TYPE_NOSNIFF = True
-SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+# SECURE_HSTS_SECONDS = 60 * 60 * 24 * 7 * 52  # one year
+# SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+# SECURE_SSL_REDIRECT = True
+# SECURE_BROWSER_XSS_FILTER = True
+# SECURE_CONTENT_TYPE_NOSNIFF = True
+# SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
-SESSION_COOKIE_SECURE = True
+# SESSION_COOKIE_SECURE = True
 
 
 # ==============================================================================
