396f5abd45ab14267d11b94a649aefeea95a7282
Ed Chalstrey <edwardchalstrey@gmail.com>
2023-10-12 10:58:39 +0100

    check for env file to use local db

--- manage.py
+++ manage.py
@@ -2,16 +2,18 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from pathlib import Path
 
 
 def main():
     """Run administrative tasks."""
-    # if os.path.exists(".env"):
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+    local_env_path = str(Path.cwd()) + "/seshat/settings/.env"
+    if os.path.exists(local_env_path):
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE',
                               'seshat.settings.local')
-    # else:
-    #     os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-    #                           'seshat.settings.production')
+    else:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                              'seshat.settings.production')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
